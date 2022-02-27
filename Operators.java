0a18dd6c6250a8840a1834367e0cd049aa5e0bf2
Siva S <68813056+SivaSJS@users.noreply.github.com>
2022-02-27 20:39:03 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Operators {
+    //Arithmetic operators -> mathematical terms such as + , - , / , % ,*
+    // Relational operators -> != , <=, >= , ==, < , >
+    // Unary operator -> just a operand of sign + and - such as pre increment and post increment
+    // eg: int val = 1; ++ val ( pre-increment ) , val++ ( post increment ), --val ( pre-decrement ), val-- ( post decrement)
+    // Logical operators -> check the condition of both sides [ && is strict and it is AND , || is OR )
+    // Bitwise operators -> [ bitwise AND &, bitwise OR | , bitwise XOR ^ , bitwise NOT ~ ]
+    // Ternary Operator -> condition ? true : false;
+    //Assignment operator -> +=, -=,*=,==
+    //shift operator ->  [ << left shift ], [ >> Right shift ], [ >>> Unsigned right shift ]
+}
